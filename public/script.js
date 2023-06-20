e15e68164b3dcf64caa7276dc4fc57b967fa4539
AmirmohammadBeheshti <bamir9707@gmail.com>
2023-06-20 16:00:04 +0330

    initial proj

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const socket = io("/");
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+socket.on("user-connected", (userId) => {
+  console.log("User Connected : ", userId);
+});
